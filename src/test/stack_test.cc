6d8ac24e57ae820acb3484e0b1a67552de4d14cd
Edwin Evia <edwinevi@student.21-school.ru>
2023-04-27 12:17:12 +0300

    test for MoveConstructor

--- src/test/stack_test.cc
+++ src/test/stack_test.cc
@@ -27,3 +27,19 @@ TEST(StackTest, CopyConstructor) {
     stack_2.pop();
     EXPECT_DOUBLE_EQ(stack_2.top(), 1231.4);
 }
+
+TEST(StackTest, MoveConstructor) {
+    s21::Stack<double> stack_1 = {1231.4, 1.342, 3.23, 4.4};
+    s21::Stack<double> stack_2(std::move(stack_1));
+    EXPECT_EQ(stack_2.size(), 4);
+    EXPECT_EQ(stack_1.size(), 0);
+
+    EXPECT_DOUBLE_EQ(stack_2.top(), 4.4);
+    stack_2.pop();
+    EXPECT_DOUBLE_EQ(stack_2.top(), 3.23);
+    stack_2.pop();
+    EXPECT_DOUBLE_EQ(stack_2.top(), 1.342);
+    stack_2.pop();
+    EXPECT_DOUBLE_EQ(stack_2.top(), 1231.4);
+
+}
