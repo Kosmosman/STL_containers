9138431f7a574b9d498a5dad51cc36315ffef3dd
Edwin Evia <edwinevi@student.21-school.ru>
2023-04-27 12:10:49 +0300

    test for InitializerListConstructor

--- src/test/stack_test.cc
+++ src/test/stack_test.cc
@@ -5,7 +5,12 @@ TEST(StackTest, DefaultConstructor) {
     EXPECT_EQ(stack.size(), 0);
 }
 
-// TEST(StackTest, DefaultConstructor) {
-//     s21::Stack<int> stack;
-//     EXPECT_EQ(stack.size(), 0);
-// }
+TEST(StackTest, InitializerListConstructor) {
+    s21::Stack<char> stack = {'c', 'b', 'a'};
+    EXPECT_EQ(stack.size(), 3);
+    EXPECT_EQ(stack.top(), 'a');
+    stack.pop();
+    EXPECT_EQ(stack.top(), 'b');
+    stack.pop();
+    EXPECT_EQ(stack.top(), 'c');
+}
