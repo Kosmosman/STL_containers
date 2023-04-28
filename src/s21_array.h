a9ef48d0a6864a9933a65872f01e3448b41378e9
Edwin Evia <edwinevi@student.21-school.ru>
2023-04-28 15:16:02 +0300

    init array

---
+++ src/s21_array.h
@@ -0,0 +1,17 @@
+#ifndef SRC_S21_ARRAY_H_
+#define SRC_S21_ARRAY_H_
+namespace s21 {
+template <typename T>
+class Array {
+ public:
+  using value_type = T;
+  using reference = T&;
+  using const_reference = const T&;
+  using iterator = T*;
+  using const_iterator = const T*;
+  using size_type = size_t;
+};
+};  // namespace s21
+
+#include "s21_array.tpp"
+#endif  // SRC_S21_QUEUE_H_
