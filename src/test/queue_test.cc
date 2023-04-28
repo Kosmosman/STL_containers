ef5bd8abf657c5721304be2400d5ed978e5837ad
Edwin Evia <edwinevi@student.21-school.ru>
2023-04-28 11:39:27 +0300

    add test for move constroct and operator

--- src/test/queue_test.cc
+++ src/test/queue_test.cc
@@ -13,37 +13,53 @@ TEST(QueueTest, InitializerListConstructor) {
 }
 
 TEST(QueueTest, CopyConstructor) {
-
+  s21::Queue<char> queue_1 = {'a', 'b', 'c', 'd'};
+  s21::Queue<char> queue_2(queue_1);
+  EXPECT_EQ(queue_2.front(), 'a');
+  EXPECT_EQ(queue_2.back(), 'd');
+  EXPECT_EQ(queue_2.size(), 4);
 }
 
 TEST(QueueTest, MoveConstructor) {
+  s21::Queue<char> queue_1 = {'a', 'b', 'c', 'd'};
+  s21::Queue<char> queue_2(std::move(queue_1));
+  EXPECT_EQ(queue_2.front(), 'a');
+  EXPECT_EQ(queue_2.back(), 'd');
+  EXPECT_EQ(queue_2.size(), 4);
 
+  EXPECT_EQ(queue_1.size(), 0);
 }
 
 TEST(QueueTest, AssignmentOperatorMoving) {
+  s21::Queue<char> queue_1 = {'a', 'b', 'c', 'd'};
+  s21::Queue<char> queue_2 = std::move(queue_1);
+  EXPECT_EQ(queue_2.front(), 'a');
+  EXPECT_EQ(queue_2.back(), 'd');
+  EXPECT_EQ(queue_2.size(), 4);
 
+  EXPECT_EQ(queue_1.size(), 0);
 }
 
-TEST(QueueTest, TopElement) {
+// TEST(QueueTest, TopElement) {
 
-}
+// }
 
-TEST(QueueTest, Empty) {
+// TEST(QueueTest, Empty) {
 
-}
+// }
 
-TEST(QueueTest, Size) {
+// TEST(QueueTest, Size) {
 
-}
+// }
 
-TEST(QueueTest, Push) {
+// TEST(QueueTest, Push) {
 
-}
+// }
 
-TEST(QueueTest, Pop) {
+// TEST(QueueTest, Pop) {
 
-}
+// }
 
-TEST(QueueTest, Swap) {
+// TEST(QueueTest, Swap) {
 
-}
+// }
