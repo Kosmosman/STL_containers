b90fd91bf75c875ecf6b5f6b7420c37e83f96264
stonehom <stonehom@student.21-school.ru>
2023-05-14 22:55:47 +0300

    new repo stonehom

---
+++ src/s21_containers/unit_tests/s21_vector_test.cc
@@ -0,0 +1,287 @@
+#include "../vector/s21_vector.h"
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+TEST(test_vectorector, constructor_default) {
+  s21::Vector<char> test_vector;
+  std::vector<char> std_test_vector;
+  EXPECT_EQ(std_test_vector.size(), test_vector.size());
+}
+
+TEST(test_vectorector, constructor_parametrized) {
+  s21::Vector<int> test_vector(5);
+  std::vector<int> std_test_vector(5);
+  EXPECT_EQ(std_test_vector.size(), test_vector.size());
+}
+
+TEST(test_vectorector, constructor_initializer_list) {
+  std::initializer_list<int> il1{1, 2, 3, 4, 5};
+  s21::Vector<int> test_vector(il1);
+  std::vector<int> std_test_vector(il1);
+  EXPECT_EQ(test_vector.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector.at(2), std_test_vector.at(2));
+  EXPECT_EQ(test_vector.at(3), std_test_vector.at(3));
+  EXPECT_EQ(test_vector.at(4), std_test_vector.at(4));
+}
+
+TEST(test_vectorector, constructor_copy) {
+  std::initializer_list<int> il1{1, 2, 3, 4, 5};
+  s21::Vector<int> test_vector(il1);
+  s21::Vector<int> copy_vector(test_vector);
+  std::vector<int> std_test_vector(il1);
+  std::vector<int> std_copy_vector(std_test_vector);
+  EXPECT_EQ(copy_vector.at(0), std_copy_vector.at(0));
+  EXPECT_EQ(copy_vector.at(1), std_copy_vector.at(1));
+  EXPECT_EQ(copy_vector.at(2), std_copy_vector.at(2));
+  EXPECT_EQ(copy_vector.at(3), std_copy_vector.at(3));
+  EXPECT_EQ(copy_vector.at(4), std_copy_vector.at(4));
+}
+
+TEST(test_vectorector, constructor_move) {
+  std::initializer_list<int> il1{1, 2, 3, 4, 5};
+  s21::Vector<int> test_vector(il1);
+  s21::Vector<int> copy_vector(std::move(test_vector));
+  std::vector<int> std_test_vector(il1);
+  std::vector<int> std_copy_vector(std::move(std_test_vector));
+  EXPECT_EQ(copy_vector.at(0), std_copy_vector.at(0));
+  EXPECT_EQ(copy_vector.at(1), std_copy_vector.at(1));
+  EXPECT_EQ(copy_vector.at(2), std_copy_vector.at(2));
+  EXPECT_EQ(copy_vector.at(3), std_copy_vector.at(3));
+  EXPECT_EQ(copy_vector.at(4), std_copy_vector.at(4));
+}
+
+TEST(test_vectorector, constructor_operator) {
+  s21::Vector<int> test_vector_1 = {1, 2, 3, 4, 5};
+  s21::Vector<int> test_vector_2;
+  test_vector_2 = std::move(test_vector_1);
+  std::vector<int> std_test_vector = {1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector_2.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector_2.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector_2.at(2), std_test_vector.at(2));
+  EXPECT_EQ(test_vector_2.at(3), std_test_vector.at(3));
+  EXPECT_EQ(test_vector_2.at(4), std_test_vector.at(4));
+}
+
+TEST(VectorTest, at_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector.at(2), std_test_vector.at(2));
+  EXPECT_EQ(test_vector.at(3), std_test_vector.at(3));
+  EXPECT_EQ(test_vector.at(4), std_test_vector.at(4));
+}
+
+TEST(VectorTest, at_func_throw) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+  EXPECT_ANY_THROW(test_vector.at(1));
+  EXPECT_ANY_THROW(std_test_vector.at(1));
+}
+
+TEST(VectorTest, square_brackets_operator) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector.at(2), std_test_vector.at(2));
+  EXPECT_EQ(test_vector.at(3), std_test_vector.at(3));
+  EXPECT_EQ(test_vector.at(4), std_test_vector.at(4));
+}
+
+TEST(VectorTest, front_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.front(), std_test_vector.front());
+}
+
+TEST(VectorTest, back_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.back(), std_test_vector.back());
+}
+
+TEST(VectorTest, data_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(*test_vector.data(), *std_test_vector.data());
+}
+
+TEST(test_vectorector, empty) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+  EXPECT_EQ(test_vector.empty(), std_test_vector.empty());
+}
+
+TEST(VectorTest, size_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.size(), std_test_vector.size());
+}
+
+TEST(VectorTest, max_size_func) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+
+  EXPECT_EQ(test_vector.max_size(), std_test_vector.max_size());
+}
+
+TEST(VectorTest, reserve_func) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+  test_vector.reserve(10);
+  std_test_vector.reserve(10);
+  EXPECT_EQ(test_vector.capacity(), std_test_vector.capacity());
+}
+
+TEST(VectorTest, reserve_func_1) {
+  s21::Vector<int> test_vector(10);
+  std::vector<int> std_test_vector(10);
+  test_vector.reserve(2);
+  std_test_vector.reserve(2);
+  EXPECT_EQ(test_vector.capacity(), std_test_vector.capacity());
+}
+
+TEST(VectorTest, reserve_func_except) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+  EXPECT_THROW(test_vector.reserve(2305843009213693952), std::length_error);
+  EXPECT_THROW(std_test_vector.reserve(2305843009213693952), std::length_error);
+}
+
+TEST(VectorTest, capacity_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.capacity(), std_test_vector.capacity());
+}
+
+TEST(VectorTest, shrink_to_fit_func) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+  test_vector.reserve(10);
+  std_test_vector.reserve(10);
+  test_vector.shrink_to_fit();
+  std_test_vector.shrink_to_fit();
+  EXPECT_EQ(test_vector.capacity(), std_test_vector.capacity());
+}
+
+TEST(VectorTest, clear_func) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+  test_vector.clear();
+  std_test_vector.clear();
+  EXPECT_EQ(test_vector.size(), std_test_vector.size());
+}
+
+TEST(VectorTest, insert_func) {
+  s21::Vector<int> test_vector{1, 2, 3};
+  std::vector<int> std_test_vector{1, 2, 3};
+  test_vector.insert(test_vector.begin() + 1, 5);
+  std_test_vector.insert(std_test_vector.begin() + 1, 5);
+
+  EXPECT_EQ(test_vector.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector.at(2), std_test_vector.at(2));
+}
+
+TEST(VectorTest, erase_func) {
+  s21::Vector<int> test_vector{1, 2, 3, 4};
+  std::vector<int> std_test_vector{1, 2, 3, 4};
+
+  test_vector.erase(test_vector.begin() + 1);
+  std_test_vector.erase(std_test_vector.begin() + 1);
+
+  EXPECT_EQ(test_vector.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector.at(2), std_test_vector.at(2));
+}
+
+TEST(VectorTest, push_back_func) {
+  s21::Vector<int> test_vector;
+  std::vector<int> std_test_vector;
+
+  test_vector.push_back(2);
+  test_vector.push_back(3);
+  test_vector.push_back(4);
+  test_vector.push_back(5);
+
+  std_test_vector.push_back(2);
+  std_test_vector.push_back(3);
+  std_test_vector.push_back(4);
+  std_test_vector.push_back(5);
+
+  EXPECT_EQ(test_vector.at(0), std_test_vector.at(0));
+  EXPECT_EQ(test_vector.at(1), std_test_vector.at(1));
+  EXPECT_EQ(test_vector.at(2), std_test_vector.at(2));
+  EXPECT_EQ(test_vector.at(3), std_test_vector.at(3));
+}
+
+TEST(VectorTest, pop_back_func) {
+  s21::Vector<int> test_vector{1, 2};
+
+  test_vector.pop_back();
+  test_vector.pop_back();
+  test_vector.pop_back();
+  test_vector.pop_back();
+
+  EXPECT_EQ(test_vector.size(), 0);
+  EXPECT_EQ(test_vector.capacity(), 2);
+}
+
+TEST(VectorTest, swap_func) {
+  s21::Vector<int> test_vector;
+  s21::Vector<int> test_vector_copy;
+
+  std::vector<int> std_test_vector;
+  std::vector<int> std_test_vector_copy;
+
+  test_vector_copy.swap(test_vector);
+  std_test_vector_copy.swap(std_test_vector);
+
+  EXPECT_EQ(test_vector.size(), std_test_vector.size());
+  EXPECT_EQ(test_vector.capacity(), std_test_vector.capacity());
+
+  EXPECT_EQ(test_vector_copy.size(), std_test_vector_copy.size());
+  EXPECT_EQ(test_vector_copy.capacity(), std_test_vector_copy.capacity());
+}
+
+TEST(VectorTest, iterator_operator_minus_minus) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(2)--, std_test_vector.at(2)--);
+}
+TEST(VectorTest, iterator_operator_plus_plus) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(1)++, std_test_vector.at(1)++);
+}
+TEST(VectorTest, iterator_operator_plus) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(0) + test_vector.at(1),
+            std_test_vector.at(0) + std_test_vector.at(1));
+}
+TEST(VectorTest, iterator_operator_minus) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(2) - test_vector.at(1),
+            std_test_vector.at(2) - std_test_vector.at(1));
+}
+TEST(VectorTest, iterator_operator_eq) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(4) == std_test_vector.at(4), 1);
+}
+TEST(VectorTest, iterator_operator_not_eq) {
+  s21::Vector<int> test_vector{1, 2, 3, 4, 5};
+  std::vector<int> std_test_vector{1, 2, 3, 4, 5};
+  EXPECT_EQ(test_vector.at(3) != std_test_vector.at(4), 1);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ src/s21_containers/vector/s21_vector.h
@@ -0,0 +1,306 @@
+#ifndef SRC_S21_CONTAINERS_VECTOR_S21_VECTOR_H_
+#define SRC_S21_CONTAINERS_VECTOR_S21_VECTOR_H_
+
+#include <cmath>
+#include <initializer_list>
+#include <iostream>
+
+namespace s21 {
+template <typename T>
+class VectorIterator;
+
+template <class T>
+class VectorConstIterator;
+
+template <typename T>
+class Vector {
+ public:
+  // Vector Member type
+  using value_type = T;
+  using reference_type = T&;
+  using const_reference = const T&;
+  using iterator = VectorIterator<T>;
+  using const_iterator = const T*;
+  using size_type = std::size_t;
+
+  //  Vector Member functions
+  Vector() : size_(0), capacity_(0), data_(nullptr){};
+
+  explicit Vector(size_type n)
+      : size_(n), capacity_(size_), data_(new value_type[capacity_]()){};
+
+  Vector(std::initializer_list<value_type> const& items)
+      : size_{items.size()},
+        capacity_(items.size()),
+        data_{new value_type[capacity_]} {
+    std::copy(items.begin(), items.end(), data_);
+  }
+
+  Vector(const Vector& v) {
+    if (this != &v) {
+      this->size_ = v.size_;
+      this->capacity_ = size_;
+      this->data_ = new value_type[capacity_];
+      for (size_type i = 0; i < v.size_; i++) {
+        data_[i] = v.data_[i];
+      }
+    }
+  }
+
+  Vector(Vector&& v) noexcept
+      : size_(v.size_), capacity_(v.capacity_), data_(v.data_) {
+    v.bring_to_zero();
+  }
+
+  ~Vector() { this->remove(); }
+
+  Vector& operator=(Vector&& v) noexcept {
+    if (this != &v) {
+      this->remove();
+      this->size_ = v.size_;
+      this->capacity_ = v.capacity_;
+      this->data_ = v.data_;
+      v.bring_to_zero();
+    }
+    return *this;
+  }
+
+  // Vector Element access
+  reference_type at(size_type pos) {
+    if (pos >= this->size()) {
+      throw std::out_of_range("Index is out of range");
+    }
+    return data_[pos];
+  }
+
+  reference_type operator[](size_type pos) {
+    if (pos < (size_type)0 || pos > size_) {
+      throw std::out_of_range("Index is out ot range");
+    }
+    return data_[pos];
+  }
+
+  const_reference front() { return *data_; }
+
+  const_reference back() { return *(data_ + size_ - 1); }
+
+  iterator data() { return data_; }
+
+  // Vector Iterators
+  iterator begin() { return iterator(data_); }
+
+  iterator end() { return iterator(data_ + size_); }
+
+  // Vector Capacity
+  bool empty() { return (size_ == 0); }
+
+  size_type size() { return size_; }
+
+  size_type max_size() {
+    int bits = 63;
+    if (sizeof(void*) == 4) {
+      bits = 31;
+    }
+    return static_cast<size_type>(pow(2, bits)) / sizeof(value_type) - 1;
+    ;
+  }
+
+  void reserve(size_type size) {
+    if (size > max_size()) {
+      throw std::length_error("Size is too large");
+    }
+    if (size > this->size_) {
+      this->allocate(size);
+    }
+  }
+
+  size_type capacity() { return capacity_; }
+
+  void shrink_to_fit() {
+    if (size_ < capacity_) {
+      allocate(size_);
+    }
+  }
+
+  // Vector Modifiers
+  void clear() { size_ = 0; }
+
+  iterator insert(iterator pos, const_reference value) {
+    size_type position = &(*pos) - data_;
+    size_type zero = 0;
+    if (position < zero || position > size_) {
+      throw std::out_of_range("Index is out ot range");
+    }
+    if (size_ + 1 >= capacity_) {
+      this->allocate(size_ + size_ / 2 + 1);
+    }
+    value_type replace = data_[position];
+    size_++;
+    data_[position] = value;
+    for (size_type i = position + 1; i < size_; ++i) {
+      value_type next = data_[i];
+      data_[i] = replace;
+      replace = next;
+    }
+    return data_ + position;
+  }
+
+  void erase(iterator pos) {
+    size_type position = &(*pos) - data_;
+    size_type zero = 0;
+    if (position < zero || position > size_) {
+      throw std::out_of_range("Index is out ot range");
+    }
+    for (size_type i = position + 1; i < size_; i++) {
+      data_[i - 1] = data_[i];
+    }
+    size_--;
+  }
+
+  void push_back(const_reference value) {
+    if (size_ >= capacity_) {
+      allocate(size_ + size_ / 2 + 1);
+    }
+    data_[size_++] = value;
+  }
+
+  void pop_back() {
+    if (size_ > 0) {
+      size_--;
+    }
+  }
+
+  void swap(Vector& other) {
+    using std::swap;
+    swap(this->size_, other.size_);
+    swap(this->capacity_, other.capacity_);
+    swap(this->data_, other.data_);
+  }
+
+ private:
+  size_type size_;
+  size_type capacity_;
+  value_type* data_;
+
+  void allocate(size_type new_cap) {
+    value_type* new_data = new value_type[new_cap];
+    for (size_type i = 0; i < size_; ++i) {
+      new_data[i] = data_[i];
+    }
+    delete[] data_;
+    data_ = new_data;
+    capacity_ = new_cap;
+  }
+
+  void bring_to_zero() {
+    this->size_ = 0;
+    this->capacity_ = 0;
+    this->data_ = nullptr;
+  }
+
+  void remove() {
+    if (this->data_ != nullptr) {
+      delete[] this->data_;
+    }
+    this->size_ = 0;
+    this->capacity_ = 0;
+    this->data_ = nullptr;
+  }
+};
+
+template <typename T>
+class VectorIterator {
+  friend class Vector<T>;
+
+ public:
+  using value_type = T;
+  using pointer_type = T*;
+  using reference_type = T&;
+
+  VectorIterator() { ptr_ = nullptr; }
+  VectorIterator(pointer_type ptr) : ptr_(ptr) {}
+
+  VectorIterator& operator++() {
+    ptr_++;
+    return *this;
+  }
+  VectorIterator operator++(int) {
+    VectorIterator tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+  VectorIterator& operator--() {
+    ptr_--;
+    return *this;
+  }
+  VectorIterator operator--(int) {
+    VectorIterator tmp = *this;
+    --(*this);
+    return tmp;
+  }
+  VectorIterator operator+(const size_t value) {
+    VectorIterator tmp(this->ptr_ + value);
+    return tmp;
+  }
+  VectorIterator operator-(const size_t value) {
+    VectorIterator tmp(this->ptr_ - value);
+    return tmp;
+  }
+  bool operator==(const VectorIterator& other) const {
+    return ptr_ == other.ptr_;
+  }
+  bool operator!=(const VectorIterator& other) const {
+    return !(*this == other);
+  }
+  reference_type operator*() const { return (*ptr_); }
+  pointer_type operator->() { return ptr_; }
+
+ private:
+  pointer_type ptr_;
+};
+
+template <typename T>
+class VectorConstIterator {
+  friend class Vector<T>;
+
+ public:
+  using value_type = T;
+  using pointer_type = T*;
+  using reference_type = T&;
+
+  VectorConstIterator() { ptr_ = nullptr; }
+  VectorConstIterator(pointer_type ptr) : ptr_(ptr) {}
+
+  VectorConstIterator& operator++() {
+    ptr_++;
+    return *this;
+  }
+  VectorConstIterator operator++(int) {
+    VectorIterator tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+  VectorConstIterator& operator--() {
+    ptr_--;
+    return *this;
+  }
+  VectorConstIterator operator--(int) {
+    VectorConstIterator tmp = *this;
+    --(*this);
+    return tmp;
+  }
+  bool operator==(const VectorConstIterator& other) const {
+    return ptr_ == other.ptr_;
+  }
+  bool operator!=(const VectorConstIterator& other) const {
+    return !(*this == other);
+  }
+  reference_type operator*() const { return (*ptr_); }
+  pointer_type operator->() { return ptr_; }
+
+ private:
+  pointer_type ptr_;
+};
+}  // namespace s21
+
+#endif  // SRC_S21_CONTAINERS_VECTOR_S21_VECTOR_H_
