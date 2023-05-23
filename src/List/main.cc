e0793cda29bcf11fbe02aefbeeab308fb9d85329
cherigra <nikitakrasovskij5@gmail.com>
2023-05-23 10:44:05 +0300

    fix(s21_list): copy construct doesn't work

--- src/List/main.cc
+++ src/List/main.cc
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
 #include "s21_list.h"
 
 int main() {
-    s21::list<int> test {1, 2}; /// TODO error
+//    s21::list<int> test {1, 2}; /// TODO error
 
+    s21::list<int> my_list{1, 2, 3};
+    s21::list<int> my_list_copy(my_list);
+//    std::list<int> std_list{1, 2, 3};
+//    std::list<int> std_list_copy(std_list);
 
-    //------------
-//    s::list<int> testOriginal;
-//    for (auto item: test) {
+//    for (const auto &item:std_list) {
+//
 //        std::cout << item << " ";
 //    }
-//
-//    auto begin = test.begin();
-//    auto end = test.end();
+
 
     s21::list<int>::ListIterator it;
-    for (it = test.begin(); it != test.end(); ++it) {
+    for (it = my_list.begin(); it != my_list.end(); ++it) {
         const auto& item = *it;
         std::cout << item << " ";
     }
