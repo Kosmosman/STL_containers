cf80f13d1686b59b306885b135d090c5f2cdb3a8
Edwin Evia <edwinevi@student.21-school.ru>
2023-06-06 12:12:09 +0300

    tests.cc : add check apple os for free

--- src/unit_test/tests.cc
+++ src/unit_test/tests.cc
@@ -1651,6 +1651,8 @@ TEST_F(S21StackTest, EmplaceFront) {
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
+#ifdef __APPLE__
   free(__cxxabiv1::__cxa_get_globals());
+#endif
   return RUN_ALL_TESTS();
 }
